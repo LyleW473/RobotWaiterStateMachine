52df92b8b11611e0c93e53e6b33994239a6f8ec0
Gee-Lyle Wong (k22039642) <k22039642@nmslab-004098.nms.kcl.ac.uk>
2024-04-23 18:42:29 +0100

    Added navigation to a specified point using userdata, input keys and output keys

--- main_node.py
+++ main_node.py
@@ -9,33 +9,47 @@ def main():
     rospy.init_node("main_node")
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
+    sm = smach.StateMachine(outcomes=['succeeded', 'aborted'], input_keys=["current_stage", "stages"])
 
     class PointA(smach.State):
-        def __init__(self):
-            smach.State.__init__(self, outcomes=["success"])
-            self.counter = 0
+        def __init__(self, outcomes, input_keys, output_keys):
+            smach.State.__init__(self, outcomes=outcomes, input_keys=input_keys, output_keys=output_keys)
         def execute(self, userdata):
             rospy.loginfo("point A")
-            return "success"
-
-    class PointB(smach.State):
-        def __init__(self):
-            smach.State.__init__(self, outcomes=["success"])
-        def execute(self, userdata):
-            rospy.loginfo("point B")
-            return "success"
-
-    class PointC(smach.State):
-        def __init__(self):
-            smach.State.__init__(self, outcomes=["success"])
-        def execute(self, userdata):
-            rospy.loginfo("point C")
+            userdata.current_stage += 1
+            rospy.loginfo(f"Current stage: {userdata.current_stage}")
             return "success"
 
     with sm:
 
+        sm.userdata.current_stage = 0
+        sm.userdata.stages = [
+                                {
+                                 "position":{"x": 6.14, "y": 3.198230504989624},
+                                 "orientation":{"z": -0.71, "w": 0.71}
+                                }
+                                # Stage(
+                                #     stage_number=1,
+                                #     position={"x": 6.14, "y": 3.198230504989624},
+                                #     orientation={"z": -0.71, "w": 0.71}
+                                #     ),
+                                # Stage(
+                                #     stage_number=2,
+                                #     position={"x": 6.14, "y": 8.42},
+                                #     orientation={"z": -0.71, "w": 0.71}
+                                #     ),
+                                # Stage(
+                                #     stage_number=3,
+                                #     position={"x": 1.956, "y": 2.90412},
+                                #     orientation={"z": -0.71, "w": 0.71}
+                                #     ),
+                                ]
+
         def test_goal_cb(userdata, goal):
+
+            rospy.loginfo(userdata.keys())
+            current_stage = userdata.current_stage
+
             my_goal = MoveBaseGoal()
 
             # Header
@@ -43,23 +57,41 @@ def main():
             my_goal.target_pose.header.stamp = rospy.Time.now()
 
             # Pos
-            my_goal.target_pose.pose.position.x = 6.14
-            my_goal.target_pose.pose.position.y = 3.198230504989624
+            my_goal.target_pose.pose.position.x = userdata.stages[current_stage]["position"]["x"]
+            my_goal.target_pose.pose.position.y = userdata.stages[current_stage]["position"]["y"]
             my_goal.target_pose.pose.position.z = 0
 
             # Orientation
-            my_goal.target_pose.pose.orientation.z = -0.71
-            my_goal.target_pose.pose.orientation.w = -0.71
+            my_goal.target_pose.pose.orientation.z = userdata.stages[current_stage]["orientation"]["z"]
+            my_goal.target_pose.pose.orientation.w = userdata.stages[current_stage]["orientation"]["w"]
 
             return my_goal
 
         smach.StateMachine.add("NAVIGATE",
-                               SimpleActionState("move_base", MoveBaseAction, goal_cb=test_goal_cb),
-                               transitions={"succeeded": "POINT_A", "aborted": "POINT_B", "preempted": "POINT_C"}
+                               SimpleActionState("move_base",
+                               MoveBaseAction,
+                               goal_cb=test_goal_cb,
+                               input_keys = ["current_stage", "stages"]
+
+                               ),
+                               transitions = {"succeeded": "POINT_A", "aborted": "NAVIGATE", "preempted": "NAVIGATE"},
+                               remapping={
+                                   "current_stage": "current_stage",
+                                   "stages": "stages"
+                               }
                                )
-        smach.StateMachine.add("POINT_A", PointA(), transitions={"success": "succeeded"})
-        smach.StateMachine.add("POINT_B", PointB(), transitions={"success": "aborted"})
-        smach.StateMachine.add("POINT_C", PointC(), transitions={"success": "aborted"})
+        smach.StateMachine.add(
+                               "POINT_A",
+                               PointA(
+                                    outcomes=["success"],
+                                    input_keys=["current_stage"],
+                                    output_keys=["current_stage"]
+                                    ),
+                               transitions={"success": "succeeded"},
+                               remapping={"current_stage": "current_stage"}
+                               )
+
+        rospy.loginfo(sm.userdata.current_stage)
 
     sis = IntrospectionServer("my_server", sm, "SM_ROOT")
     sis.start()
