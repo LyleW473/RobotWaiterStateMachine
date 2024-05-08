f220cce4c636120de6703666b697c99006ae447d
Gee-Lyle <wlyle473@gmail.com>
2024-05-08 20:30:36 +0100

    Added validation for request data in "WaitForRequest" state

--- wait_for_request.py
+++ wait_for_request.py
@@ -10,6 +10,7 @@ class WaitForRequest(State):
         State.__init__(self, outcomes=outcomes, input_keys=input_keys, output_keys=output_keys)
         self.data = None
         self.requests_counter = 0  # Pointer to which request we are expecting
+        self.known_foods = set(["broccoli", "pizza", "sandwich"])
 
     def check_for_reset(self, userdata):
         # Out of sync
@@ -28,9 +29,15 @@ class WaitForRequest(State):
         if self.data is None:
             return "failed"
         else:
+            # Check if the food is recognised
+            food_name = self.data.food_name.lower()
+            if food_name not in self.known_foods:
+                return "failed"
+            person_name = self.data.person_name.lower().capitalize()
+
             userdata.status_type = "succeeded"
-            userdata.request_data["person_name"] = self.data.person_name
-            userdata.request_data["food_name"] = self.data.food_name
+            userdata.request_data["person_name"] = person_name
+            userdata.request_data["food_name"] = food_name
             rospy.loginfo("Received data on topic /food_request!")
             rospy.loginfo(f" {self.data.person_name} | {self.data.food_name}")
             return "succeeded"
